3bb0e6f681a84008e0009d85a565e47ad64ffc17
beanu <zhaoyunhello@gmail.com>
2017-03-09 16:35:43 +0800

    增加购物车模块

---
+++ l3_shoppingcart/src/main/java/com/beanu/l3_shoppingcart/DemoActivity.java
@@ -0,0 +1,13 @@
+package com.beanu.l3_shoppingcart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+}
