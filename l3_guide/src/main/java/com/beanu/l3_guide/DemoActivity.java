06b1de48f2955c9271597a7331f49ed89c8fa5a4
beanu <zhaoyunhello@gmail.com>
2017-03-23 15:34:14 +0800

    添加引导页模块

---
+++ l3_guide/src/main/java/com/beanu/l3_guide/DemoActivity.java
@@ -0,0 +1,19 @@
+package com.beanu.l3_guide;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class DemoActivity extends AppCompatActivity {
+
+    private Button mBtnStyle1;
+    private Button mBtnStyle2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+
+    }
+}
